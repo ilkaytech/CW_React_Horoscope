17fbaa169ba568f56642e0b57796225b067db944
ilkay <06ilkayozturk@gmail.com>
2023-07-05 19:29:27 +0300

    header end

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.scss";
+const Header = () => {
+  return (
+    <div className="header">
+      <span className="text-shadow">Who are you?</span>
+    </div>
+  );
+};
+
+export default Header;

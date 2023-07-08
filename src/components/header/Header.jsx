0bd33868f9203c90fe2ff933b2116883862ac27c
ilkay <06ilkayozturk@gmail.com>
2023-07-08 16:55:18 +0300

    edit scss

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.scss";
 const Header = () => {
   return (
     <div className="header">
-      <span className="text-shadow">Who are you?</span>
+      <span className="text-shadow">Who Are You?</span>
     </div>
   );
 };
